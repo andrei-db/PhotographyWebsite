e3d5438ec557356a2e6335e78774a10d4dd7985b
AndreiDaraban <andrei.daraban70@yahoo.com>
2022-05-26 02:50:17 +0300

    some big changes
    -deleted team section, changed weoofer section to about section
    -made navbar, header text and about section responsive
    -created basic contact section

--- js/script.js
+++ js/script.js
@@ -1,5 +1,31 @@
-$(document).ready(function(){
+const navlinks = document.querySelector("body header nav div");
 
-    
+$(document).ready(function () {
+
+  window.onscroll = function () { myFunction() };
+  var navbar = document.getElementById("#nav");
+  var sticky = navbar.offsetTop;
+
+
+  function myFunction() {
+    if (window.pageYOffset >= sticky) {
+      
+      navbar.classList.add("make-it-white");
+      navlinks.classList.remove("nav-links");
+      
+
+    } else {
+      navbar.classList.remove("make-it-white");
+      navlinks.classList.add("nav-links");
+    }
+  }
+
+ 
+var iconEvent=document.getElementById("icon");
+iconEvent.addEventListener("click",function(){
+  alert("gfd");
+});
+
   
-  });
+
+});
